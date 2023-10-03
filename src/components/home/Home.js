7d669bcb5ca5349721969a635a6742352363a381
RamanSaini <ramansaini068@gmail.com>
2023-10-03 15:24:01 +0530

    json file

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     return (
         <React.Fragment>
             <div className="overflow-hidden container-fluid">
-                <img className="img-fluid  position-relative" src="/assets/images/agent.jpg" />
+                <img className="img-fluid  position-relative" src="%PUBLIC_URL%/assets/images/agent.jpg" />
                 <div className="position-absolute top-left"> <p className="text-wrap heading text-white "  >Start Your Story with Olvera</p>
                     <Button className="contact-btn rounded-pill px-4 py-2 " style={{ background: "#c2278e", border: "#c2278e" }} size="sm" ><Link to="/contact-us" className="text-decoration-none text-light">CONTACT US</Link></Button>
                 </div>
